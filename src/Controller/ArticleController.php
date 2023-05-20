6fc86a50902f470ab403700c5a63d3f0a80f1c70
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2023-05-20 15:39:53 +0200

    affichage de l'article et des ses composants

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ArticleController extends AbstractController
 {
     #[Route('/article/{slug}', name: 'article_show')]
-    public function show(): Response
+    public function show(?Article $article): Response
     {
+
+        if (!$article) {
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('article/show.html.twig', [
-            'controller_name' => 'ArticleController',
+            'article' => $article,
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepo): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepo->findAll()
         ]);
     }
 }
