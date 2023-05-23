7a88dad06886891433ef7f2ec7eecb14856686af
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2023-05-23 17:31:52 +0200

    création des sous-menus

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\Comment;
+use App\Entity\Menu;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -44,6 +45,12 @@ class DashboardController extends AbstractDashboardController
             MenuItem::linkToCrud('tous les articles', 'fas fa-newspaper', Article::class),
             MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
             MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class)
+        ]);
+        yield MenuItem::subMenu('Menu', 'fas fa-list')->setSubItems([
+            MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class)->setQueryParameter('submenuIndex', 0),
+            MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class)->setQueryParameter('submenuIndex', 1),
+            MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class)->setQueryParameter('submenuIndex', 2),
+            MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class)->setQueryParameter('submenuIndex', 3)
         ]);
         yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
     }
--- src/Controller/Admin/MenuCrudController.php
+++ src/Controller/Admin/MenuCrudController.php
@@ -3,23 +3,91 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Menu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Menu\SubMenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class MenuCrudController extends AbstractCrudController
 {
+    const MENU_PAGES = 0;
+    const MENU_ARTICLES = 1;
+    const MENU_LINKS = 2;
+    const MENU_CATEGORIES = 3;
+
+    public function __construct(
+        private RequestStack $requestStack
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Menu::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        $subMenuIndex = $this->getSubMenuIndex();
+        //return parent::configureCrud($crud);
+        $entityLabelInSingular = 'un menu';
+
+        $entityLabelInPlural = match ($subMenuIndex) {
+            self::MENU_ARTICLES => 'Articles',
+            self::MENU_CATEGORIES => 'Catégories',
+            self::MENU_LINKS => 'Liens personnalisés',
+            default => 'Pages'
+        };
+        return $crud
+            ->setEntityLabelInSingular($entityLabelInSingular)
+            ->setEntityLabelInPlural($entityLabelInPlural);
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $subMenuIndex = $this->getSubMenuIndex();
+
+        yield IdField::new('name', 'Titre de la navigation');
+
+        yield NumberField::new('menuOrder', 'Ordre');
+
+        yield $this->getFieldFromSubMenuIndex($subMenuIndex)
+            ->setRequired(true);
+
+        yield BooleanField::new('isVisible', 'Visible');
+
+        yield AssociationField::new('subMenus', 'Sous-élèments');
     }
-    */
+
+    private function getFieldFromSubMenuIndex(int $subMenuIndex)
+    {
+        $fieldName = match ($subMenuIndex) {
+            self::MENU_ARTICLES => 'article',
+            self::MENU_CATEGORIES => 'Category',
+            self::MENU_LINKS => 'link',
+            default => 'page'
+        };
+        return ($fieldName == 'link') ? TextField::new($fieldName, 'lien') : AssociationField::new($fieldName);
+    }
+
+
+
+    private function getSubMenuIndex(): int
+    {
+        $query = $this->requestStack->getMainRequest()->query;
+
+        if ($referer = $query->get('referrer')) {
+            parse_str(parse_url($referer, PHP_URL_QUERY), $query);
+
+            return $query['submenuIndex'] ?? 0;
+        }
+
+        return $query->getInt('submenuIndex');
+        // return $this->requestStack->getMainRequest()->query->getInt('submenuIndex');
+    }
 }
